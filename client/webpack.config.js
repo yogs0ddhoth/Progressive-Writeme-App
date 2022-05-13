e94a8282a6ef4ae0b3de8a4280985e2cad847b8d
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-05-12 20:43:00 -0400

    html webpack, manifest, inject manifest configured

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,49 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin ({ template: './index.html', title: 'Progressive-Writeme-App' }),
+
+      // generate manifest.js
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Progressive-Writeme-App',
+        short_name: 'PWA',
+        description: 'Just another text editor',
+        // background_color: '#ffffff',
+        // theme_color: '#ffffff',
+        crossorigin: 'use-credentials',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          },
+        ],
+      }),
+      // create use sw.js to create service worker with WebpackPwaManifest
+      new InjectManifest({ swSrc: './src-sw.js', swDest: 'src-sw.js' })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        }
       ],
     },
   };
