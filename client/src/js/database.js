8d309760a35e4553f11cefcf206add16a83ae24b
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-05-14 09:10:50 -0400

    Create Read indexeddb operations functional

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,23 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => { // CREATE
+  const jateDb = await openDB('jate', 1); // connect to 'jate' database
+  const tx = jateDb.transaction('jate', 'readwrite'); // create readwrite transaction in 'jate' database
+  const store = tx.objectStore('jate'); // connect to 'jate' object store
+  const result = await store.add( // store content
+    { content: content }
+  );
+  console.log('data saved to the database', result); // confirmation
+};
+
+export const getDb = async () => { // READ
+  const jateDb = await openDB('jate', 1) // connect to 'jate' database
+  const tx = jateDb.transaction('jate', 'readonly'); // create 'readeonly transaction in the 'jate' database
+  const store = tx.objectStore('jate'); // connect to 'jate' object store
+  const result = await store.getAll(); // get all contents in the database
+  console.log('data saved to the database', result); // confirmation
+};
 
 initdb();
